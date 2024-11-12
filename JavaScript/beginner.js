bb3be839cec8f29cdfdc16a3953ea46660f8f142
Nsikak Akpan <builtbykaksy@gmail.com>
2024-11-12 17:43:22 +0100

    updates

--- JavaScript/beginner.js
+++ JavaScript/beginner.js
@@ -6,3 +6,6 @@ const h1 = document.querySelector('h1');
 h1.addEventListener('click', function() {
     alert('I have been clicked with event listener 😁😂');
 })
+
+console.log("My day one of learning JavaScript...")
+console.log("external javascript file running...")
