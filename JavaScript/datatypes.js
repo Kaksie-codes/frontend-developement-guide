61cc7c7fbd9e684c74a9af1ad867c2f274c10092
Nsikak Akpan <builtbykaksy@gmail.com>
2024-11-12 15:48:42 +0100

    changes

---
+++ JavaScript/datatypes.js
@@ -0,0 +1,43 @@
+// string Datatype
+let string1 = "Happy Hour";
+console.log(string1);
+let string2 = 'Paper towel';
+console.log(string2);
+let string3 = `Candy Crush`;
+console.log(string3);
+console.log(typeof string3);
+
+// number Datatype
+let num1 = 10;
+console.log(num1);
+console.log(typeof num1);
+
+// boolean Datatype
+let bool1 = true;
+console.log(bool1);
+console.log(typeof bool1);
+
+// undefined Datatype
+let undef;
+console.log(undef);
+console.log(typeof undef);
+
+// null Datatype
+let nullVar = null;
+console.log(nullVar);
+console.log(typeof nullVar);
+
+// bigint Datatype
+let bigInt1 = 1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000n;
+console.log(bigInt1);
+console.log(typeof bigInt1);
+
+// array Datatype
+let arr = [1, 2, 3, 4, 5];
+console.log(arr);
+console.log(typeof arr);
+
+// object Datatype
+let obj = {name: "John Doe", age: 30};
+console.log(obj);
+console.log(typeof obj);
