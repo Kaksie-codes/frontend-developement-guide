8ad25e2d32220ef40533d7a29a7e316aa4558653
Nsikak Akpan <builtbykaksy@gmail.com>
2024-11-26 18:15:11 +0100

    Update arrays.js

--- JavaScript/arrays.js
+++ JavaScript/arrays.js
@@ -1,12 +1,37 @@
-let myArray = ['cow Leg'];
-console.log(myArray);
-myArray.push('Spaghetti');
-console.log(myArray);
-myArray.push('Indomie');
-console.log(myArray);
-myArray.unshift('Milo');
-console.log(myArray);
-myArray.shift();
-console.log(myArray);
-myArray.pop();
-console.log(myArray);
+// let myArray = ['cow Leg'];
+// console.log(myArray);
+// myArray.push('Spaghetti');
+// console.log(myArray);
+// myArray.push('Indomie');
+// console.log(myArray);
+// myArray.unshift('Milo');
+// console.log(myArray);
+// myArray.shift();
+// console.log(myArray);
+// myArray.pop();
+// console.log(myArray);
+// myArray.unshift(10);
+// console.log(myArray);
+
+// let myNumbers = [];
+// console.log(myNumbers);
+
+// for(let i = 1; i <= 10; i++){
+//     myNumbers.push(i);
+//     console.log(myNumbers);
+// }
+
+// for(let i = 1; i <= 10; i++){
+//     myNumbers.pop();
+//     console.log(myNumbers);
+// }
+
+// console.log(Math.random())
+// console.log(Math.sqrt(9))
+
+let randomNumber = Math.random();
+console.log({randomNumber})
+randomNumber = randomNumber * 20;
+console.log({randomNumber})	
+randomNumber = Math.round(randomNumber)
+console.log({randomNumber})	
