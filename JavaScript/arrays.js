35627117668cb3a229bbbbea0d1a093002722849
Nsikak Akpan <builtbykaksy@gmail.com>
2024-11-28 17:00:08 +0100

    Update arrays.js

--- JavaScript/arrays.js
+++ JavaScript/arrays.js
@@ -1,17 +1,17 @@
-// let myArray = ['cow Leg'];
-// console.log(myArray);
-// myArray.push('Spaghetti');
-// console.log(myArray);
-// myArray.push('Indomie');
-// console.log(myArray);
-// myArray.unshift('Milo');
-// console.log(myArray);
-// myArray.shift();
-// console.log(myArray);
-// myArray.pop();
-// console.log(myArray);
-// myArray.unshift(10);
-// console.log(myArray);
+let myArray = ['cow Leg'];
+console.log(myArray);
+myArray.push('Spaghetti');
+console.log(myArray);
+myArray.push('Indomie');
+console.log(myArray);
+myArray.unshift('Milo');
+console.log(myArray);
+myArray.shift();
+console.log(myArray);
+myArray.pop();
+console.log(myArray);
+myArray.unshift(10);
+console.log(myArray);
 
 // let myNumbers = [];
 // console.log(myNumbers);
@@ -29,9 +29,30 @@
 // console.log(Math.random())
 // console.log(Math.sqrt(9))
 
-let randomNumber = Math.random();
-console.log({randomNumber})
-randomNumber = randomNumber * 20;
-console.log({randomNumber})	
-randomNumber = Math.round(randomNumber)
-console.log({randomNumber})	
+// let randomNumber = Math.random();
+// console.log({randomNumber})
+// randomNumber = randomNumber * 20;
+// console.log({randomNumber})	
+// randomNumber = Math.round(randomNumber)
+// console.log({randomNumber})
+
+
+// let myAnimals = ["monkey", "Zebra", ["Lion", "ox", ["horse", ['rat', ['chicken']]],], "mouse", "tiger", "cat", "pig", "dog"]
+
+// console.log(myAnimals)
+// console.log(myAnimals.length)
+// console.log(myAnimals[2])
+// console.log(myAnimals[myAnimals.length - 1])
+// console.log(myAnimals[2][2][1][1][0])
+
+// const animals = ["monkey", "Zebra", "Lion", "Antelope", "eagle"]
+
+// for(let n = 0; n < specialArray.length; n++){
+//     console.log(specialArray[n]);
+// }
+
+// for(let animal of animals){
+//     console.log(animal)
+// }
+
+// animals.forEach((animal) => console.log(animal))
